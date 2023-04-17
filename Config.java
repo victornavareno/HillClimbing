670aab8a44dc75d61d132eeff90a122a48364734
doktor <113288139+victornavareno@users.noreply.github.com>
2023-04-17 11:47:51 +0200

    Final form of the Config file

--- Config.java
+++ Config.java
@@ -1,8 +1,17 @@
 package RobotPackage;
 public class Config {
-	public final static String ficheroElegido = "LABECOIN8.txt";    // INTRODUCIR AQUÍ EL FICHERO A LEER
-	public final static int limiteNodosGenerados = 500; // MAXIMO NUMERO DE NODOS A EXPLORAR
-	public final static int modoResolucion = 1;  // DEFINO LA ESTRATEGIA EMPLEADA -  0: ESCALADA SIMPLE
-																				//   1: ESCALADA MAXIMA PENDIENTE
+	
+	//**IMPORTANTE: SE RECOMIENDA UTILIZAR EL TEMA OSCURO EN TU ENTORNO DE DESARROLLO**
+	
+	//parámetros de ejecución importantes:
+	public final static String ficheroElegido = "LABECOIN1.txt";  // INTRODUCIR AQUÍ EL FICHERO A LEER
+	
+	public final static int limiteNodosGenerados = 800; // MAXIMO NUMERO DE NODOS A EXPLORAR
+	
+	public final static int modoResolucion = 0;  // 0: ESCALADA SIMPLE | 1: ESCALADA MAXIMA PENDIENTE
+	
+	
+	
+	// NO ES NECESARIO MODIFICAR ESTE,  SÓLO TIENE FINES ESTÉTICOS AL IMPRIMIR LA SOLUCION
 	public final static int saltoLinea = 20;  // Numero de movimientos imprimidos por linea
 }
