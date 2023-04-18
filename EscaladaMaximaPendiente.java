66e1190475342c98dd0d6be5c445e44bb6e8faad
doktor <113288139+victornavareno@users.noreply.github.com>
2023-04-18 18:59:55 +0200

    Fixed maximum slope algorithm
    Added a new heuristic, less strict than the previous one that allows to find more solutions in a reasonable amount of time

--- EscaladaMaximaPendiente.java
+++ EscaladaMaximaPendiente.java
@@ -2,6 +2,9 @@ package RobotPackage;
 
 import java.util.*;
 
+//AUTOR:    VICTOR ANDRES NAVAREÑO MOZA
+//PROYECTO: LABECOIN, INTELIGENCIA ARTIFICIAL Y SISTEMAS INTELIGENTES
+
 public class EscaladaMaximaPendiente extends Robot {
 
 	public EscaladaMaximaPendiente(int[][] matrizLaberinto, int _objetivoMonedas) {
@@ -11,38 +14,72 @@ public class EscaladaMaximaPendiente extends Robot {
 	@Override
 	public void buscarMonedas() {
 		List<int[]> direccion = getListaMovimientosDisponibles();
-		int indiceMejorOpcion = 0;
-		int valorMejorOpcion = 0;
-		
+		List<Integer> posibilidades = new ArrayList<Integer>();
+		int valorMejorOpcion = -1;
+		Random rand = new Random();
+
 		// guardan las coordenadas de la mejor opcion:
 		int posiX = 0; 
 		int posiY = 0;
 
-		for(int i = 0; i < direccion.size(); i++) {
+		int mejorOpcionX = 0;
+		int mejorOpcionY = 0;
+		while(direccion.size() > 0) {
 			this.nodosGenerados++; // estamos generando otro nodo de nuestro arbol de exploracion
+			int random = rand.nextInt(direccion.size());
 
-			int posX = posicionRobot[0] + direccion.get(i)[0];
-			int posY = posicionRobot[1] + direccion.get(i)[1];
-			
-			if((estadoActual[posX][posY] < 7) && estadoActual[posX][posY] >= 0) {
-				if((estadoActual[posX][posY] >= valorMejorOpcion) && estadoActual[posX][posY] != -1) {
-					indiceMejorOpcion = i;
+			int posX = posicionRobot[0] + direccion.get(random)[0];
+			int posY = posicionRobot[1] + direccion.get(random)[1];
+
+
+			if((estadoActual[posX][posY] < 7)) {
+				posibilidades.add(estadoActual[posX][posY]);
+				if((estadoActual[posX][posY] > valorMejorOpcion) && estadoActual[posX][posY] != -1) {
+					mejorOpcionX = direccion.get(random)[0];
+					mejorOpcionY = direccion.get(random)[1];
 					valorMejorOpcion = estadoActual[posX][posY];
 					posiX = posX;
 					posiY = posY;
 				}
 			}
-			else if(i == 7 && (estadoActual[posX][posY] == -1 || estadoActual[posX][posY] == 9)) {
-				this.haySolucion=false;
+			direccion.remove(random); // borro este explorado
+		}
+		//Vemos si estamos rodeados de -1 y  muros:
+		if(!hayPosibilidades(posibilidades)) {
+			resetearVecinos(); // borramos los -1 para que el robot pruebe otra ruta si nos quedamos sin posibilidades
+		}
+		else {
+			estadoActual[posicionRobot[0]][posicionRobot[1]] = -1; 
+			this.monedasConseguidas += valorMejorOpcion; // le sumo las monedas de la mejor opcion posible
+			estadoActual[posiX][posiY] = 8; // muevo el robot
+			encontrarRobot(); // actualizo la posicion del robot
+			direccion = getListaMovimientosDisponibles();
+			traducirMovimiento(mejorOpcionX, mejorOpcionY); // añado el movimiento a nuestro vector solucion
+		}
+	}
+
+	// Analiza los nodos generados en esta iteracion, si todas las posibilidades desplegadas son -1, devuelve false () 
+	private boolean hayPosibilidades(List<Integer> posibilidades ) {
+		for(int integer : posibilidades) {
+			if(integer != -1) {
+				return true;
 			}
 		}
-		//System.out.println( " ELEGIDO  " + indiceMejorOpcion);
+		return false;
+	}
 
-		estadoActual[posicionRobot[0]][posicionRobot[1]] = -1; 
-		this.monedasConseguidas += valorMejorOpcion; // le sumo las monedas de la mejor opcion posible
-		estadoActual[posiX][posiY] = 8; // muevo el robot
-		encontrarRobot(); // actualizo la posicion del robot
-		traducirMovimiento(direccion.get(indiceMejorOpcion)[0], direccion.get(indiceMejorOpcion)[1]); // añado el movimiento a nuestro vector solucion
+	// Reseteo los vecinos, poniendolos a 0, se utiliza cuando nos quedamos 
+	// sin posiblidades de exploracion (estamos rodeados de -1 y 9)
+	// Sirve para salir de un callejon sin salida, haciendo que nuestra heuristica sea menos estricta
+	private void resetearVecinos() {
+		List<int[]> direccion = getListaMovimientosDisponibles();
+		for (int i = 0; i < direccion.size(); i++) {
+			int posX = posicionRobot[0] + direccion.get(i)[0];
+			int posY = posicionRobot[1] + direccion.get(i)[1];
+			if(estadoActual[posX][posY] < 7) {
+				estadoActual[posX][posY] = 0;
+			}
+		}
 	}
 
 }
